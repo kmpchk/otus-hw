08d05aaa3948da4720b034ae41ef8e803afc2c8a
kmpchk <kmpchk@gmail.com>
2022-10-03 21:56:42 +0300

    Add test version

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,63 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+//package main
+
+import (
+	"sort"
+	"strings"
+)
+
+type WordsFreq struct {
+	Key   string
+	Value int
+}
+
+func Top10(inputString string) []string {
+
+	words := strings.Fields(inputString)
+
+	var wordsFreqMap map[string]int = make(map[string]int)
+
+	for _, word := range words {
+		wordsFreqMap[word]++
+	}
+
+	//fmt.Println(wordsFreqMap)
+
+	var wordsFreq []WordsFreq = make([]WordsFreq, 0)
+
+	//fmt.Println(wordsFreq)
+
+	for key, value := range wordsFreqMap {
+		wordsFreq = append(wordsFreq, WordsFreq{key, value})
+	}
+
+	/*for _, wordFreq := range wordsFreq {
+		fmt.Printf("Key: %s, Value: %d\n", wordFreq.Key, wordFreq.Value)
+	}*/
+
+	//fmt.Println("\nSort...\n")
+
+	sort.Slice(wordsFreq, func(i, j int) bool {
+		if wordsFreq[i].Value == wordsFreq[j].Value {
+			return wordsFreq[i].Key < wordsFreq[j].Key
+		}
+		return wordsFreq[i].Value > wordsFreq[j].Value
+	})
+
+	/*for _, wordFreq := range wordsFreq {
+		fmt.Printf("Key: %s, Value: %d\n", wordFreq.Key, wordFreq.Value)
+	}*/
+
+	var top10words []string = make([]string, 0)
+	for _, wordFreq := range wordsFreq {
+		top10words = append(top10words, wordFreq.Key)
+	}
+
+	//fmt.Println(top10words)
+
+	if len(top10words) >= 10 {
+		return top10words[:10]
+	}
+	return top10words
 }
