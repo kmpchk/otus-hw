7f6d1148941488a5f5a834764e23da1783158124
kmpchk <kmpchk@gmail.com>
2022-10-31 12:37:03 +0300

    Fix unpack.go

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,4 +1,4 @@
-package hw02unpackstring //hw02unpackstring
+package hw02unpackstring
 
 import (
 	"errors"
@@ -19,31 +19,45 @@ func IsStringValid(runeStr []rune) bool {
 
 	for i := 0; i < len(runeStr)-1; i++ {
 		if unicode.IsDigit(runeStr[i]) && unicode.IsDigit(runeStr[i+1]) && runeStr[i-1] != slash {
-			fmt.Println("BooM1!")
 			return false
 		}
 
-		if runeStr[i] == slash && (runeStr[i+1] != slash && !unicode.IsDigit(runeStr[i+1])) {
-			fmt.Println("BooM2!")
+		if runeStr[i] == slash && runeStr[i+1] != slash && !unicode.IsDigit(runeStr[i+1]) {
 			if len(runeStr) == 2 {
 				return false
 			}
-			// Additional check
-			if i-2 >= 0 { // `\\\a`
-				if runeStr[i-1] == slash && runeStr[i-2] == slash {
-					return false
-				} else if runeStr[i-1] == slash && runeStr[i-2] != slash {
-					return true
-				} else if runeStr[i-1] != slash && runeStr[i-2] != slash { // `qw\ne`
-					return false
-				}
-			} else { // `\\a`
-				return true
+			if i-2 >= 0 && runeStr[i-1] == slash && runeStr[i-2] == slash {
+				return false
+			}
+			if i-2 >= 0 && runeStr[i-1] == slash && runeStr[i-2] != slash {
+				return false
+			}
+			if i-2 >= 0 && runeStr[i-1] != slash && runeStr[i-2] != slash {
+				return false
 			}
 		}
 	}
 
 	return true
+}
+
+func processSlash(runeStr []rune, pos int, output *strings.Builder) int {
+	// Skip step -- default case = 1
+	offset := 1
+	if runeStr[pos+1] == slash && pos+2 < len(runeStr) {
+		if unicode.IsDigit(runeStr[pos+2]) {
+			repeatNum, _ := strconv.Atoi(string(runeStr[pos+2])) // Skip conversion error check
+			tmp := strings.Repeat(string(slash), repeatNum)
+			fmt.Fprintf(output, "%s", tmp)
+			// Skip slash (1) + next slash (1) + count (1)
+			offset = 3
+		} else {
+			fmt.Fprintf(output, "%c", slash)
+			// Skip slash (1) + next slash (1)
+			offset = 2
+		}
+	}
+	return offset
 }
 
 func Unpack(inputString string) (string, error) {
@@ -59,7 +73,7 @@ func Unpack(inputString string) (string, error) {
 	}
 
 	var output strings.Builder
-	var offset int = 0
+	var offset int
 
 	for pos := 0; pos <= len(runeStr)-1; {
 		char := runeStr[pos]
@@ -70,37 +84,19 @@ func Unpack(inputString string) (string, error) {
 			break
 		}
 
-		// Main loop
+		// Processing flow
+
 		nextChar := runeStr[pos+1]
-		//fmt.Printf("Cur = %c, Next = %c\n", char, nextChar)
 
+		switch char {
 		// Slash processing
-		if char == slash { // `\\\\4`
-			if runeStr[pos+1] == slash {
-				if pos+2 < len(runeStr) {
-					if unicode.IsDigit(runeStr[pos+2]) {
-						repeat_num, _ := strconv.Atoi(string(runeStr[pos+2])) // Skip conversion error check
-						tmp := strings.Repeat(string(slash), repeat_num)
-						fmt.Fprintf(&output, "%s", tmp)
-						// Skip slash (1) + next slash (1) + count (1)
-						offset = 3
-					} else {
-						fmt.Fprintf(&output, "%c", slash)
-						// Skip slash (1) + next slash (1)
-						fmt.Println(output.String())
-						offset = 2
-					}
-				}
-			} else {
-				// Skip slash (1)
-				offset = 1
-			}
-
-		} else {
-			// General execution flow
+		case slash:
+			offset = processSlash(runeStr, pos, &output)
+		// General execution flow
+		default:
 			if unicode.IsDigit(nextChar) {
-				repeat_num, _ := strconv.Atoi(string(nextChar)) // Skip conversion error check
-				tmp := strings.Repeat(string(char), repeat_num)
+				repeatNum, _ := strconv.Atoi(string(nextChar)) // Skip conversion error check
+				tmp := strings.Repeat(string(char), repeatNum)
 				fmt.Fprintf(&output, "%s", tmp)
 				// Skip char (1) + count (1)
 				offset = 2
@@ -113,10 +109,10 @@ func Unpack(inputString string) (string, error) {
 
 		// Update pos
 		pos += offset
-		fmt.Printf("Pos = %d, Offset = %d\n", pos, offset)
+		// fmt.Printf("Pos = %d, Offset = %d\n", pos, offset)
 	}
 
-	//fmt.Println(len(runeStr))
+	// fmt.Println(len(runeStr))
 	fmt.Println(output.String())
 	return output.String(), nil
 }
