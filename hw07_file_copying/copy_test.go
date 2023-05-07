4c0d70f2b8ab7f17c05190046b13c2e3432e4d02
kmpchk <kmpchk@gmail.com>
2023-05-07 17:54:18 +0300

    Add more tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -43,4 +43,35 @@ func TestCopy(t *testing.T) {
 		}
 		require.EqualError(t, err, ErrUnsupportedFile.Error())
 	})
+
+	t.Run("input path equal to output path", func(t *testing.T) {
+		// do copying
+		err := Copy("testdata/input.txt", "testdata/input.txt", 0, 100000)
+		if err != nil {
+			log.Println(err)
+		}
+		require.EqualError(t, err, ErrPathCmp.Error())
+	})
+
+	t.Run("positive full file copying", func(t *testing.T) {
+		// do copying
+		err := Copy("testdata/input.txt", "testdata/test_offset0_limit0.txt", 0, 0)
+		require.NoError(t, err)
+		expectedFile, err := os.ReadFile("testdata/test_offset0_limit0.txt")
+		require.NoError(t, err)
+		actualFile, err := os.ReadFile("testdata/out_offset0_limit0.txt")
+		require.NoError(t, err)
+		require.Equal(t, expectedFile, actualFile)
+	})
+
+	t.Run("positive partitial file copying", func(t *testing.T) {
+		// do copying
+		err := Copy("testdata/input.txt", "testdata/test_offset6000_limit1000.txt", 6000, 1000)
+		require.NoError(t, err)
+		expectedFile, err := os.ReadFile("testdata/test_offset6000_limit1000.txt")
+		require.NoError(t, err)
+		actualFile, err := os.ReadFile("testdata/out_offset6000_limit1000.txt")
+		require.NoError(t, err)
+		require.Equal(t, expectedFile, actualFile)
+	})
 }
