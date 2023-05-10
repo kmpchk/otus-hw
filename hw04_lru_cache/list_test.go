1e016b298202a2fc3b664ea59d7703078cfae64c
kmpchk <kmpchk@gmail.com>
2023-05-10 21:49:27 +0300

    Add list test

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,40 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("complex2", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10)       // [10]
+		l.PushBack(20)        // [10, 20]
+		l.PushFront(30)       // [30, 10, 20]
+		elem := l.Back().Prev // 10
+		l.Remove(elem)        // [30, 20]
+		require.Equal(t, 2, l.Len())
+
+		// require.Equal(t, (*ListItem)(nil), l.Back().Next)
+		require.Nil(t, l.Back().Next)
+
+		require.Equal(t, 30, l.Front().Value)
+		require.Equal(t, 20, l.Back().Value)
+
+		l.MoveToFront(l.Back()) // [20, 30]
+
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 30, l.Back().Value)
+
+		for idx := 0; idx < 5; idx++ {
+			l.PushFront(idx)
+		} // [4, 3, 2, 1, 0, 20, 30]
+
+		l.Remove(l.Front().Next)      // [4, 2, 1, 0, 20, 30]
+		l.MoveToFront(l.Front().Next) // [2, 4, 1, 0, 20, 30]
+
+		elems := make([]int, 0, l.Len())
+		for i := l.Front(); i != nil; i = i.Next {
+			elems = append(elems, i.Value.(int))
+		}
+
+		require.Equal(t, []int{2, 4, 1, 0, 20, 30}, elems)
+	})
 }
